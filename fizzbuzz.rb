67035a8e0f787a5164b4e6280f96a499e51154c6
Sean Oughton <seanoughton@gmail.com>
2018-01-05 19:51:20 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,5 @@
 def fizzbuzz(int)
-    if int % 3 == 0 
+  if int % 3 == 0 
     "Fizz"
-  elsif int % 5 == 0 
-    "Buzz"
-  elsif (int % 3 == 0) && (int % 5 == 0)
-    "FizzBuzz"
-  else
-    nil
+  end
 end
